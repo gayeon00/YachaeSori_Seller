5070c7b6319f697e78aa5cab8132db633fc8114c
gayeon00 <chlrkdus4819@gmail.com>
2024-02-11 03:33:40 +0900

    feat: 상품 수정 기능 구현
    1) storage에 회사소개 이미지, 손질법 이미지 넣기
    2) storage 링크 를 db에 넣기
    진행중
    
    현재상황: 넣기까진 일단 성공했는데, 소비자용 앱에서 불러오기가 안됨
    -> storage에 넣는 imageRef.putFile이 안되는듯
    해결!!
    
    다른 문제: 기존 사진에 덮어씌웠을 때 새 이미지가 폰에 안뜸...
    그리고생각해보니 그냥 storage에만 image폴더 아래 yachae_intro라고 올리고, 불러올 때도 storage에서만 받아오면 되는거아닌가?

--- app/src/main/java/com/yachaesori/yachaesori_seller/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/yachaesori/yachaesori_seller/ui/menu/MenuFragment.kt
@@ -1,14 +1,22 @@
 package com.yachaesori.yachaesori_seller.ui.menu
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.yachaesori.yachaesori_seller.MainActivity
 import com.yachaesori.yachaesori_seller.R
 import com.yachaesori.yachaesori_seller.databinding.FragmentMenuBinding
 import com.yachaesori.yachaesori_seller.ui.product.ProductViewModel
@@ -21,11 +29,26 @@ class MenuFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val fragmentMenuBinding get() = _fragmentMenuBinding!!
+    private var selectedButton: Int = -1
+
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK && result.data != null) {
+                val uri = result.data?.data
+
+                uri?.let {
+                    when (selectedButton) {
+                        R.id.buttonGoToIntroImageSetting -> uploadImage(it, "intro")
+                        R.id.buttonGoToPrepImageSetting -> uploadImage(it, "guide")
+                    }
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _fragmentMenuBinding = FragmentMenuBinding.inflate(layoutInflater)
         return fragmentMenuBinding.root
     }
@@ -44,7 +67,6 @@ class MenuFragment : Fragment() {
                         item.isChecked = true
                         linearLayoutProductManage.visibility = View.VISIBLE
                         linearLayoutOrderMagage.visibility = View.GONE
-                        linearLayoutCS.visibility = View.GONE
                         linearLayoutSetting.visibility = View.GONE
                     }
 
@@ -53,24 +75,14 @@ class MenuFragment : Fragment() {
                         //주문관리 페이지로 이동
                         linearLayoutProductManage.visibility = View.GONE
                         linearLayoutOrderMagage.visibility = View.VISIBLE
-                        linearLayoutCS.visibility = View.GONE
                         linearLayoutSetting.visibility = View.GONE
 
-                    }
-
-                    R.id.item_cs -> {
-                        item.isChecked = true
-                        linearLayoutProductManage.visibility = View.GONE
-                        linearLayoutOrderMagage.visibility = View.GONE
-                        linearLayoutCS.visibility = View.VISIBLE
-                        linearLayoutSetting.visibility = View.GONE
                     }
 
                     R.id.item_setting -> {
                         item.isChecked = true
                         linearLayoutProductManage.visibility = View.GONE
                         linearLayoutOrderMagage.visibility = View.GONE
-                        linearLayoutCS.visibility = View.GONE
                         linearLayoutSetting.visibility = View.VISIBLE
                     }
                 }
@@ -93,15 +105,63 @@ class MenuFragment : Fragment() {
                 //주문 배송관리 페이지로 이동
                 findNavController().navigate(R.id.action_item_menu_to_item_manage_order)
             }
-            buttonGoToManageNotice.setOnClickListener {
-                //공지관리 페이지로 이동
+            buttonGoToIntroImageSetting.setOnClickListener {
+                //회사소개이미지 업로드 하도록 갤러리 사진선택으로 이동
+                selectedButton = R.id.buttonGoToIntroImageSetting
+                openGallery()
             }
-            buttonGoToManageInq.setOnClickListener {
-                //문의 관리 페이지로 이동
-            }
-            buttonGoToSetting.setOnClickListener {
-                //설정 페이지로 이동
+
+            buttonGoToPrepImageSetting.setOnClickListener {
+                //손질법이미지 업로드 하도록 갤러리 사진선택으로 이동
+                selectedButton = R.id.buttonGoToPrepImageSetting
+                openGallery()
             }
         }
     }
+
+    private fun openGallery() {
+        val galleryIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+                type = "image/*"
+                putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/*"))
+            }
+
+        galleryLauncher.launch(galleryIntent)
+    }
+
+    private fun uploadImage(uri: Uri, path: String) {
+        val imageRef =
+            FirebaseStorage.getInstance().reference.child("image/yachae_${path}.jpg")
+
+        //firebase storage에 이미지 업로드
+        imageRef.putFile(uri)
+            .addOnSuccessListener {
+                Log.d("menu", "storage upload 성공")
+                // 이미지 업로드 성공 시 이미지의 키 그대로 실시간 데이터베이스에 저장
+                saveImagePathToDatabase("image/yachae_${path}.jpg", "${path}ImageUrl")
+            }
+            .addOnFailureListener {
+
+            }
+
+    }
+
+    private fun saveImagePathToDatabase(storagePath: String, childPath: String) {
+        val imageDatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("yachae").child(childPath)
+
+        // 이미지 경로를 데이터베이스에 저장
+        imageDatabaseReference.setValue(storagePath)
+            .addOnSuccessListener {
+                // 이미지 경로 저장 성공에 대한 처리
+                (activity as MainActivity).showSnackBar("성공적으로 변경됐습니다.")
+            }
+            .addOnFailureListener { exception ->
+                // 이미지 경로 저장 실패에 대한 처리
+                (activity as MainActivity).showSnackBar("변경에 실패했습니다. 다시 시도해주세요.")
+                Log.d("saveImagePathToDatabase", exception.toString())
+            }
+    }
+
+
 }
