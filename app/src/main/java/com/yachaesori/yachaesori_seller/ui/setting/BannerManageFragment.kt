9871fe65265d6807d329c49a4440e8083f2aa51e
gayeon00 <chlrkdus4819@gmail.com>
2024-02-16 18:03:10 +0900

    feat: 편집, 편집완료 사이 전환까지 완료

--- app/src/main/java/com/yachaesori/yachaesori_seller/ui/setting/BannerManageFragment.kt
+++ app/src/main/java/com/yachaesori/yachaesori_seller/ui/setting/BannerManageFragment.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,17 +17,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import com.yachaesori.yachaesori_seller.R
 import com.yachaesori.yachaesori_seller.data.model.Banner
 import com.yachaesori.yachaesori_seller.databinding.FragmentBannerManageBinding
 import com.yachaesori.yachaesori_seller.databinding.RowBannerBinding
+import com.yachaesori.yachaesori_seller.databinding.RowBannerEditBinding
 import com.yachaesori.yachaesori_seller.util.setImageFromUrl
 
 class BannerManageFragment : Fragment() {
     private lateinit var binding: FragmentBannerManageBinding
     private val bannerViewModel: BannerViewModel by viewModels { BannerViewModelFactory() }
     private val bannerListAdapter = BannerListAdapter()
+    private val bannerListEditAdapter = BannerListEditAdapter()
+
+    //편집모드인지 아닌지
+    private var isEditMode = false
 
     private val galleryLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -52,6 +63,7 @@ class BannerManageFragment : Fragment() {
 
         bannerViewModel.bannerList.observe(viewLifecycleOwner) {
             bannerListAdapter.submitList(it)
+            bannerListEditAdapter.submitList(it)
             Log.d("what", it.toString())
         }
 
@@ -66,13 +78,46 @@ class BannerManageFragment : Fragment() {
         setNavigationIcon()
         setAddBanner()
         setBannerList()
+        setEditButton()
 
+    }
 
+    /**
+     * 편집 버튼 누르면 배너 편집 모드로
+     */
+    private fun setEditButton() {
+        binding.toolbarManageBanner.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.item_banner_edit -> {
+                    isEditMode = true
+
+                    binding.toolbarManageBanner.menu.findItem(R.id.item_banner_edit).setVisible(false)
+                    binding.toolbarManageBanner.menu.findItem(R.id.item_banner_edit_complete).setVisible(true) //완료 아이콘이 보이도록
+                    binding.listBanners.adapter = bannerListEditAdapter // 배너 목록도 갱신
+                }
+
+                R.id.item_banner_edit_complete -> {
+                    isEditMode = false
+                    binding.toolbarManageBanner.menu.findItem(R.id.item_banner_edit).setVisible(true)
+                    binding.toolbarManageBanner.menu.findItem(R.id.item_banner_edit_complete).setVisible(false) //편집 아이콘이 보이도록
+                    binding.listBanners.adapter = bannerListAdapter // 배너 목록도 갱신
+                }
+            }
+
+            true
+        }
     }
 
     private fun setBannerList() {
         binding.listBanners.run {
             adapter = bannerListAdapter
+            val divider = MaterialDividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+            //마지막 아이템은 구분선 없도록
+            divider.isLastItemDecorated = false
+            //구분선 추가
+            addItemDecoration(
+                divider
+            )
         }
     }
 
@@ -83,7 +128,7 @@ class BannerManageFragment : Fragment() {
      * 맨 마지막 순서로 추가되기
      */
     private fun setAddBanner() {
-        binding.toolbarManageBanner.setOnMenuItemClickListener {
+        binding.fabAddBanner.setOnClickListener {
             val galleryIntent =
                 Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
                     type = "image/*"
@@ -91,7 +136,6 @@ class BannerManageFragment : Fragment() {
                 }
 
             galleryLauncher.launch(galleryIntent)
-            true
         }
     }
 
@@ -132,6 +176,35 @@ class BannerListAdapter :
     override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+}
+
+class BannerListEditAdapter :
+    ListAdapter<Banner, BannerListEditAdapter.BannerEditViewHolder>(BannerDiffUtil()) {
+    inner class BannerEditViewHolder(
+        private val binding: RowBannerEditBinding
+    ) : ViewHolder(binding.root) {
+        fun bind(item: Banner) {
+            binding.run {
+                tvOrder.text = item.order.toString()
+                imageView.setImageFromUrl(item.imageUrl)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerEditViewHolder {
+        return BannerEditViewHolder(
+            RowBannerEditBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: BannerEditViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
 }
 
 class BannerDiffUtil : DiffUtil.ItemCallback<Banner>() {
